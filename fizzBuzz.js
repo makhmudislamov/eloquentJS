e3ffe1c2f71506c01a806d929eaec48c2e5a417c
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-08-29 14:46:24 -0700

    number divisible by 5 - added

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -20,6 +20,8 @@
 for (let number = 1; number <= 100; number++) {
     if (number % 3) {
         console.log("Fizz");
+    } else if (number % 5) {
+        console.log("Buzz");      
     }
        
 }
