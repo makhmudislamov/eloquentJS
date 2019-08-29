baaab941ab60f6a3ee70e12d7b704037bb8fbbe8
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-08-29 14:31:36 -0700

    added for loop solution

--- loopingTriangle.js
+++ loopingTriangle.js
@@ -10,11 +10,19 @@
     #######
 */
 
+/*
+WHILE LOOP SOLUTION
 let triangleHash = "#";
 while ( triangleHash.length <= 7 ) {
     console.log(triangleHash);
     triangleHash += "#"
-
 }
+*/ 
 
+/*
+FOR LOOP SOLUTION
+for (let triangleHash = "#"; triangleHash.length <= 7; triangleHash += "#") {
+    console.log(triangleHash);
+}
+*/
 
