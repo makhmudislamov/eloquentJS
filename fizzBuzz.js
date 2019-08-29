2b6450ee6096a4caf990cf6f1a27650e0003203a
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-08-29 15:08:16 -0700

    if/else solution

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -18,15 +18,12 @@
 // print FizzBuzz
 
 for (let number = 1; number <= 100; number++) {
-    if (number % 3 == 0) {
-        console.log(number + "Fizz");
+    if (number % 3 == 0 && number % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (number % 3 == 0) {
+        console.log("Fizz");      
     } else if (number % 5 == 0) {
-        console.log(number + "Buzz");      
-    } else if () {
-        // console.log("LALALALALALALA");   
-        console.log(number);
-           
-    }
-       
+        console.log("Buzz");              
+    }     
 }
 
