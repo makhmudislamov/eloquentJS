3f6e6fd429f0bcc3e874bfc75763eda298ac6c30
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-08-29 14:56:45 -0700

    bug fix

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -18,10 +18,14 @@
 // print FizzBuzz
 
 for (let number = 1; number <= 100; number++) {
-    if (number % 3) {
-        console.log("Fizz");
-    } else if (number % 5) {
-        console.log("Buzz");      
+    if (number % 3 == 0) {
+        console.log(number + "Fizz");
+    } else if (number % 5 == 0) {
+        console.log(number + "Buzz");      
+    } else if () {
+        // console.log("LALALALALALALA");   
+        console.log(number);
+           
     }
        
 }
