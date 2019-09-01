20e1b7c0dca9c04051d8ac21081dd8a55cb7f48a
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-01 11:37:05 -0700

    solution 2

--- minimum.js
+++ minimum.js
@@ -3,8 +3,20 @@
     Optional: write the function in 3 different ways: 
 */
 
-// const that holds a function value
+// function 1: const that holds a function value
+/* 
 const minimum = function (num1, num2) {
     return Math.min(num1, num2);
 }
 console.log(minimum(123, 4812332));
+*/
+
+// function 2: declaring a function minimum
+/* 
+function minimum (num1, num2) {
+    return Math.min(num1, num2)
+}
+console.log(minimum(123, 42));
+*/
+
+
