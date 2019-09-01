37ce387a76d6dd6d84b7c19999181a198f66e20d
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-01 12:28:14 -0700

    added if/else statements - 1st attempt

--- recursion.js
+++ recursion.js
@@ -2,3 +2,16 @@
     Define a recursive function isEven corresponding to this description. 
     The function should accept a single parameter (a positive, whole number) and return a Boolean.
 */
+
+
+const isEven = num => {
+    
+    // if (num < 0) {
+    //     return false;
+    // } 
+    if (isEven(num - 2) == 0)
+        return true;
+
+}
+
+console.log(isEven(50));
