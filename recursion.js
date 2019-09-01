6461951bb669f7f4872584b2033c9a861c5fdcb5
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-01 11:58:47 -0700

    added problem descrip

---
+++ recursion.js
@@ -0,0 +1,4 @@
+/* 
+    Define a recursive function isEven corresponding to this description. 
+    The function should accept a single parameter (a positive, whole number) and return a Boolean.
+*/
