4138d7ba60806b8d92c792a3b869912372484729
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-03 19:24:03 -0700

    added optional step = 2

--- rangeSum.js
+++ rangeSum.js
@@ -11,14 +11,14 @@ old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. M
 values so that range(5, 2, -1) produces [5, 4, 3, 2].
 */
 
-const range = (start, end, step) => {
+const range = (start, end, step = 1) => {
     let rangeArray = [];
      
         if (step > 0) {
             for (let num = start; num <= end; num += step) {
                 rangeArray.push(num);
             }
-        } else {
+        } else if (step < 0) {
             for (let num = start; num <= end; num -= step) {
                 rangeArray.push(num);
             }
@@ -36,5 +36,5 @@ const sum = (arr) => {
     return sumOfArray;
 } 
 
-console.log(range(1,10, 2));
-console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(range(1, 10 ));
+console.log(sum(range(1,10)));
