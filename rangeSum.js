f8a2e0e06cbb6ceeabd0825758410996dd904164
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-03 23:18:37 -0700

    output is big number - infintie loop

--- rangeSum.js
+++ rangeSum.js
@@ -18,9 +18,13 @@ const range = (start, end, step = 1) => {
             for (let num = start; num <= end; num += step) {
                 rangeArray.push(num);
             }
-        } else { // need to fix this condition
-            for (let num = start; num <= end; num -= step) {
-                rangeArray.push(num);
+        } else if (step < 0){ // need to fix this condition
+            for (let num = start; num >= end; num -= step) {
+                // rangeArray.unshift(num);
+                console.log(num);
+                
+                
+                
             }
         }
 
@@ -36,5 +40,5 @@ const sum = (arr) => {
     return sumOfArray;
 } 
 
-console.log(range(10, 1, -1));
-console.log(sum(range(1,10)));
+console.log(range(10, 2, -1));
+// console.log(sum(range(1,10)));
